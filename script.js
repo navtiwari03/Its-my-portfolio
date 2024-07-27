8a54c42c3f25e25bf72508b6c77e2ce6fd95c7fa
navtiwari03 <naveenkumartiwari381@gmail.com>
2024-07-28 02:34:06 +0530

    text edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menu = document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
 menu.oneclick = () => {
     menu.classList.toggle('bx-x');
@@ -7,14 +7,14 @@ menu.oneclick = () => {
 }
 
 window.onscroll = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 }
-const typed = new Typed ('.multiple-text',{
-    strings:['Frontend Developer', 'Backend Developer','Web Designer','Photographer'],
-    typeSpeed:80,
-    backSpeed:80,
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer', 'App Developer','Web Designer','Photographer'],
+    typeSpeed: 80,
+    backSpeed: 80,
     backDelay:1200,
-    loop: true,
-});
+    loop:true,
+  });
